91390701bc7d05c4292d68a974afb846c7f8472b
Your Name <you@example.com>
2022-12-26 13:17:02 +0300

    sprint_1 fix

--- mystore/mystore/settings.py
+++ mystore/mystore/settings.py
@@ -187,17 +187,6 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 MAIL_FROM_ADDRESS = os.environ.get('MAIL_FROM_ADDRESS')
 
 
-# # Aws config
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-#
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
-# AWS_S3_REGION_NAME = "us-east-2"
-# AWS_S3_SIGNATURE_VERSION = "s3v4"
-#
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Основной url для управления медиафайлами
 MEDIA_URL = '/media/'
