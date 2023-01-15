51cd1d73cdb7062bbd7ead11f903d39b3c4380a5
Your Name <you@example.com>
2023-01-15 03:31:32 +0300

    migration

--- mystore/store/models.py
+++ mystore/store/models.py
@@ -5,49 +5,83 @@ from django.template.defaultfilters import slugify
 from .utils import image_resize
 
 
-class Product(models.Model):
-      name = models.CharField(max_length=50, null=False)
-      slug = models.SlugField(max_length=20, null=False, unique=True)
-      image = models.ImageField(upload_to='profile_pics')
-      price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
-      qty = models.PositiveSmallIntegerField(null=False, default=1, help_text='Customer quantity initialization')
-      inventory = models.PositiveSmallIntegerField(null=False)
-      description = models.TextField(max_length=200, null=False)
-      created_at = models.DateTimeField(default=timezone.now)
-      updated_at = models.DateTimeField(auto_now=True)
+class Products(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    slug = models.SlugField(max_length=20, null=False, unique=True)
+    image = models.ImageField(upload_to='profile_pics')
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    qty = models.PositiveSmallIntegerField(null=False, default=1, help_text='Customer quantity initialization')
+    inventory = models.PositiveSmallIntegerField(null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
-      class Meta:
+    class Meta:
         ordering = ['id']
 
-      def __str__(self):
+    def __str__(self):
         return self.name
 
-      def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         image_resize(self.image, 500, 500)
         if not self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
 
 
-class Rating(models.Model):
-      product = models.ForeignKey(Product, related_name='rating', on_delete=models.CASCADE)
-      one = models.PositiveIntegerField(default=0, null=True, blank=True)
-      two = models.PositiveIntegerField(default=0, null=True, blank=True)
-      three = models.PositiveIntegerField(default=0, null=True, blank=True)
-      four = models.PositiveIntegerField(default=0, null=True, blank=True)
-      five = models.PositiveIntegerField(default=0, null=True, blank=True)
+class Feedbacks(models.Model):
+    product = models.ForeignKey(Products, related_name='rating', on_delete=models.CASCADE)
+    coment = models.TextField(max_length=500, null=True)
+    one = models.PositiveIntegerField(default=0, null=True, blank=True)
+    two = models.PositiveIntegerField(default=0, null=True, blank=True)
+    three = models.PositiveIntegerField(default=0, null=True, blank=True)
+    four = models.PositiveIntegerField(default=0, null=True, blank=True)
+    five = models.PositiveIntegerField(default=0, null=True, blank=True)
 
-      class Meta:
+    class Meta:
         ordering = ['product']
 
-      def __str__(self):
-          # Extract all rating values and return max key.
-          # Reverse this list if there is a tie and you want the last key.
-          rating_list = {
+    def __str__(self):
+        # Extract all rating values and return max key.
+        # Reverse this list if there is a tie and you want the last key.
+        rating_list = {
             '1': self.one,
             '2': self.two,
             '3': self.three,
             '4': self.four,
             '5': self.five
-          }
-          return str(max(rating_list, key=rating_list.get))
+        }
+        return str(max(rating_list, key=rating_list.get))
+
+
+class Localizes(models.Model):
+    product = models.OneToOneField(Products, related_name='localize', on_delete=models.CASCADE)
+    EN_description = models.CharField(max_length=500, null=False)
+    RU_description = models.CharField(max_length=500, null=False)
+
+    def __str__(self):
+        return self.product
+
+
+class Categories(models.Model):
+    product = models.ForeignKey(Products, related_name='product', on_delete=models.PROTECT())
+    category = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.product
+
+
+class Tags(models.Model):
+    product = models.ManyToManyField(Products, related_name='product', on_delete=models.PROTECT())
+    tags = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.product
+
+
+class Discounts(models.Model):
+    product = models.ManyToManyField(Products, related_name='product', on_delete=models.PROTECT())
+    category = models.ManyToManyField(Categories, related_name='category', on_delete=models.PROTECT())
+    discount = models.PositiveIntegerField(default=0, null=False, blank=True)
+
+    def __str__(self):
+        return self.product
--- mystore/users/models.py
+++ mystore/users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
-from django.utils.translation import gettext_lazy as _
-
+from mystore.orders.models import DeliveryData
 
 phone_validator = RegexValidator(
     r'\d{3}?-?\d{3}?-?\d{4}', 'Only ten numbers and dashes allowed.')
@@ -10,13 +9,7 @@ phone_validator = RegexValidator(
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=50, blank=False, null=True, validators=[phone_validator])
-    address = models.CharField(
-        _("address"), max_length=150, blank=False, null=False)
-    city = models.CharField(_("city"), max_length=50, blank=False, null=False)
-    state = models.CharField(_("state"), max_length=5, blank=False, null=False)
-    zipcode = models.CharField(
-        _("zipcode"), max_length=5, blank=False, null=True)
+    deliveryData = models.ForeignKey(DeliveryData, on_delete=models.CASCADE)
 
 
     def __str__(self):
